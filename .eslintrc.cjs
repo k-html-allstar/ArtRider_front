74e9cfa75b2ed89e29ac83345a7e3700af0b5e50
heehminh <mink141416@gmail.com>
2024-08-05 22:37:57 +0900

    🔧 chore: prettier, storybook 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,24 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
   },
-}
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+    "plugin:storybook/recommended",
+    "plugin:storybook/recommended"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    "no-console": "error",
+  },
+};
