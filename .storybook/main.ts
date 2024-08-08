69d52486ed602a20ec6c7848d58747eabb991c65
heehminh <mink141416@gmail.com>
2024-08-08 21:54:21 +0900

    🔧 chore: tailwind, storybook 설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,6 +8,12 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-styling",
+      options: {
+        postCss: true,
+      },
+    },
   ],
   framework: {
     name: "@storybook/react-vite",
